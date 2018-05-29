f06fd4833c3676a938d7e7cc230fd9f38ec65e3a
Subtixx <subtixx@gmail.com>
2018-05-30 01:28:14 +0200

    Fixes #1

--- src/features/hoverProvider.ts
+++ src/features/hoverProvider.ts
@@ -11,11 +11,24 @@ export class hoverProvider implements vscode.HoverProvider {
     constructor(extensionPath: string) {
         this.functions = {};
 
-        for(var i in funcDefs.defs)
+        for(let i in typeDefs.luaTypes)
         {
-            var idef = funcDefs.defs[i];
+            let idef = typeDefs.luaTypes[i];
+            if(this.functions[idef.label] == undefined)
+                this.functions[idef.label] = new vscode.MarkdownString(idef.description);
+        }
 
-            this.functions[idef.module + "." + idef.label] = idef.toMarkdown();
+        for(let i in funcDefs.defs)
+        {
+            let idef = funcDefs.defs[i];
+
+            if(this.functions[idef.module] == undefined)
+                this.functions[idef.module] = new vscode.MarkdownString(idef.module);
+
+            if(idef.module != "")
+                this.functions[idef.module + "." + idef.label] = idef.toMarkdown();
+            else
+                this.functions[idef.label] = idef.toMarkdown();
         }
 
         // Built-in lua functions (print etc.)
@@ -34,7 +47,6 @@ export class hoverProvider implements vscode.HoverProvider {
             for(let j in itype.methods)
             {
                 let jmethod = itype.methods[j];
-                // ! This will block "math" from showing when hovering over "math.sin"
                 this.functions[itype.name + "." + jmethod.name] =  jmethod.toMarkdown();
             }
         }
@@ -42,12 +54,22 @@ export class hoverProvider implements vscode.HoverProvider {
     
     provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover> {
         return new Promise<vscode.Hover>((resolve, reject) => {
+            let hoveredTypePosition = document.getWordRangeAtPosition(position);
             let hoveredWordPosition = document.getWordRangeAtPosition(position, /[\w\.]+/);
             if(hoveredWordPosition == undefined)
                 hoveredWordPosition = document.getWordRangeAtPosition(position);
+            
+            let hoveredFunction = this.functions[document.getText(hoveredTypePosition)];
+            if(hoveredFunction != undefined)
+            {
+                var hover = new vscode.Hover(hoveredFunction);
+                resolve(hover);
+                return;
+            }
 
             let hoveredWord = document.getText(hoveredWordPosition);
-            let hoveredFunction = this.functions[hoveredWord];
+            console.log(hoveredWord);
+            hoveredFunction = this.functions[hoveredWord];
             if(hoveredFunction == undefined)
             {
                 reject();
